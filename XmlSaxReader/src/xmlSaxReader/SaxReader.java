a53a05a21df85116962ef7dd8bdc7e75b6493a57
ote <javiotero93@gmail.com>
2024-10-17 12:43:50 +0200

    Replaced recursive function for while loop to avoid stacking calls. Deleted the validity checking of the path to avoid redundancy because it was already being validated when checking the file's existence. Included a try-with-resources block to close the Scanner class after its usage.

--- XmlSaxReader/src/xmlSaxReader/SaxReader.java
+++ XmlSaxReader/src/xmlSaxReader/SaxReader.java
@@ -19,62 +19,54 @@ public class SaxReader {
 	public static String EnterAndValidateRoute(Scanner input) {
 		
 		String route = null;
-		System.out.println("Enter the route for the xml file you want to read: ");
-		route = input.nextLine();
+		boolean isValid = false;
 		
-		try {
+		while (!isValid) {
 			
-			Paths.get(route);
+			System.out.println("Enter the route for the xml file you want to read: ");
+			route = input.nextLine();
 			
-		}catch (Exception e) {
-			
-			// TODO: handle exception
-			System.out.println("Couldn't validate the entered path. Please enter the route again: ");
-			EnterAndValidateRoute(input);
+			// Checking existence of path
+			File file = new File(route);
+			if (!file.exists()) {
+				
+				System.out.println("File not found, try again.");
+				
+			} else {
+				
+				isValid = true;
+				
+			}		
 			
 		}
 		
 		return route;
-
+		
 	}
 
 	public static void main(String[] args) throws FileNotFoundException, IOException, SAXException {
 		// TODO Auto-generated method stub
 
 		//VARIABLES
-		
 		SAXParser xmlParser;
-		Scanner input = new Scanner(System.in);
-		String route = EnterAndValidateRoute(input);
-		File file = new File(route);
 		
-		// Checking existence of file
-		
-		while (!file.exists()) {
+		try (Scanner input = new Scanner(System.in)) {
 			
-			System.out.println("File not found, try again.");
-			route = EnterAndValidateRoute(input);
-			file = new File(route);
+			String route = EnterAndValidateRoute(input);
 			
+			// Reading the file and showing it on console
+			
+	        try {
+	        	
+	            xmlParser = SAXParserFactory.newInstance().newSAXParser(); // Creates both parserFactory and parser (will parse through the document's lines)          
+	            ContentManager manager = new ContentManager(); // Creates the handler (will decide an action for each parsed line)
+	            InputSource fileXML = new InputSource(route); // Declares the file to read          
+	            xmlParser.parse(fileXML, manager); // Tells the parser which file to parse and which handler to use when parsing
+	            
+	        } catch (ParserConfigurationException ex) {
+	        	
+	            Logger.getLogger(SaxReader.class.getName()).log(Level.SEVERE, null, ex);
+	        }
 		}
-		
-		// Reading the file and showing it on console
-		
-        try {
-        	
-            xmlParser = SAXParserFactory.newInstance().newSAXParser(); // Creates both parserFactory and parser (will parse through the document's lines)          
-            ContentManager manager = new ContentManager(); // Creates the handler (will decide an action for each parsed line)
-            
-            InputSource fileXML = new InputSource(route); // Declares the file to read          
-            	
-            xmlParser.parse(fileXML, manager); // Tells the parser which file to parse and which handler to use when parsing
-            	
-            
-        } catch (ParserConfigurationException ex) {
-        	
-            Logger.getLogger(SaxReader.class.getName()).log(Level.SEVERE, null, ex);
-        }
-		
 	}
-
 }
